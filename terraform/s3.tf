011cd5b02c127d07c4a8b78032c55d1199a5826d
jhayashi1 <jaredhayashi@gmail.com>
2024-08-04 16:21:42 -0500

    fix: properly reference cloudfront_bucket name

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "cloudfront_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "access_block" {
-    bucket = aws_s3_bucket.cloudfront_bucket
+    bucket = aws_s3_bucket.cloudfront_bucket.id
     
     block_public_acls   = true
     block_public_policy = true
