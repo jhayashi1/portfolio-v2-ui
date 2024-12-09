88f459f96f9215e4a846c7f51f0b6462ece25ca9
jhayashi1 <jaredhayashi@gmail.com>
2024-12-08 18:17:37 -0600

    fix: syntax

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -47,7 +47,7 @@ resource "aws_route53_record" "main_cert_validation_records" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.main_zone.zone_id
+  zone_id         = aws_route53_zone.main_zone.zone_id
 }
 
 resource "aws_acm_certificate_validation" "main_cert_validation" {
